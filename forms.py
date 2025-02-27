c3f3634d22ba0aa38b707c9263d55c51729006b4
Leonardo7 <fabian.anrey@outlook.com>
2025-02-26 20:13:02 -0600

    26/02/2025 Conexion BD

---
+++ forms.py
@@ -0,0 +1,45 @@
+from wtforms import Form
+from wtforms import StringField, IntegerField, SelectField,EmailField, RadioField
+from wtforms import validators
+
+class UserForm2(Form):
+    id=IntegerField('id',
+    [validators.number_range(min=1, max=20,message='valor no valido')])
+    nombre=StringField('nombre',[
+        validators.DataRequired(message='El nombre es requerido'),
+        validators.length(min=4,max=20, message='requiere min=4 max=20')
+    ])
+   
+    apaterno=StringField('apaterno',[
+        validators.DataRequired(message='El apellido es requerido')
+    ])
+   
+    email=EmailField('correo',[
+        validators.DataRequired(message='El apellido es requerido'),
+        validators.Email(message='Ingrese un correo valido')
+    ])
+
+
+
+class UserForm(Form):
+    matricula = IntegerField("Matricula", [
+        validators.DataRequired(message="El campo es requerido"),
+    ])
+    nombre = StringField("Nombre", [
+        validators.DataRequired(message="El campo es requerido")
+    ])
+    apellido = StringField("Apellido", [
+        validators.DataRequired(message="El campo es requerido")
+    ])
+    correo = EmailField("Correo", [
+        validators.DataRequired(message="El campo es requerido")
+    ])
+
+class ZodiacoForm(Form):
+    nombre = StringField("Nombre", [validators.DataRequired("El nombre es requerido")])
+    apellido_p = StringField("Apellido Paterno", [validators.DataRequired("El apellido paterno es requerido")])
+    apellido_m = StringField("Apellido Materno", [validators.DataRequired("El apellido materno es requerido")])
+    dia = IntegerField("Día", [validators.DataRequired("El día es requerido"), validators.NumberRange(min=1, max=31, message="Día inválido")])
+    mes = IntegerField("Mes", [validators.DataRequired("El mes es requerido"), validators.NumberRange(min=1, max=12, message="Mes inválido")])
+    anio = IntegerField("Año", [validators.DataRequired("El año es requerido"), validators.NumberRange(min=1900, max=2023, message="Año inválido")])
+    sexo = RadioField("Sexo", choices=[('M', 'Masculino'), ('F', 'Femenino')], validators=[validators.DataRequired("El sexo es requerido")])
